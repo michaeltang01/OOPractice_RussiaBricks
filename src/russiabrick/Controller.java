6beed6386d354d21cb5a878242f8cce173d08fc8
Siwei <Siwei@Siwei-PC>
2017-09-12 21:35:30 +0800

    Add controller methods.

--- src/russiabrick/Controller.java
+++ src/russiabrick/Controller.java
@@ -1,4 +1,25 @@
 package russiabrick;
 
 public class Controller {
+	private int leftMoveBtn;
+	private int rightMoveBtn;
+	private int downMoveBtn;
+	private int startBtn;
+	private int stopBtn;
+	
+	public void leftMoveHandler() {
+		return;
+	}
+	public void rightMoveHandler() {
+		return;
+	}
+	public void downMoveHandler() {
+		return;
+	}
+	public void startGameHandler() {
+		return;
+	}
+	public void stopGameHandler() {
+		return;
+	}
 }
