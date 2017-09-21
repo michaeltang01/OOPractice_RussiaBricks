f40f5345e2d3831ca24d019b81823c85844a81ac
Michael Tang <michael.tang01@sap.com>
2017-09-21 14:54:15 +0800

    Add demo for JavaFX UI and KeyEvent handler.

--- src/russiabrick/GameBoard.java
+++ src/russiabrick/GameBoard.java
@@ -1,28 +1,57 @@
 package russiabrick;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+//import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.shape.*;
+import javafx.scene.paint.*;
+//import javafx.scene.canvas.*;
+import javafx.scene.*;
+import javafx.scene.input.KeyCode;  
+import javafx.scene.input.KeyEvent;
+import javafx.event.*;
+
 public class GameBoard extends Application {
 
-    @Override
+	@Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+		
+		// Add 2 rectangles for demo
+        Rectangle block_demo = new Rectangle();
+        block_demo.setX(50);
+        block_demo.setY(50);
+        block_demo.setWidth(30);
+        block_demo.setHeight(30);
+        block_demo.setFill(Color.WHITE);
+        block_demo.setStroke(Color.BLACK);
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Rectangle block_demo1 = new Rectangle();
+        block_demo1.setX(80);
+        block_demo1.setY(50);
+        block_demo1.setWidth(30);
+        block_demo1.setHeight(30);
+        block_demo1.setFill(Color.BLACK);
+        block_demo1.setStroke(Color.BLACK);
+        
+        Group root = new Group();
+        root.getChildren().add(block_demo);
+        root.getChildren().add(block_demo1);
 
-        Scene scene = new Scene(root, 300, 250);
+        // Add event handler for key pressed in scene
+        Scene scene = new Scene(root, 300, 400);
+        scene.setOnKeyReleased(new EventHandler<KeyEvent> () {
+			@Override
+			public void handle(KeyEvent event) {
+				System.out.println(" Key Released!" + event.getCode());  
+				if (event.getCode() == KeyCode.UP) {  
+					System.out.println("    You pressed UP!");  
+				}
+			}
+        }); 
+
 
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
